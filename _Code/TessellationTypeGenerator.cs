9e2d0ce47725fa4867242ff1254f2d6965987441
Yi Yang @ Ahlfeldite <ahyangyi@gmail.com>
2024-07-18 03:29:03 +0800

    (tes) hide unfinished stuff, work towards a minimal viable product

--- _Code/TessellationTypeGenerator.cs
+++ _Code/TessellationTypeGenerator.cs
@@ -4,6 +4,7 @@ using Arcen.Universal;
 using System;
 using static Arcen.AIW2.External.TextVarMap;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace AhyangyiMaps
 {
@@ -28,19 +29,74 @@ namespace AhyangyiMaps
 
         protected void InnerGenerate(Galaxy galaxy, ArcenHostOnlySimContext Context, MapConfiguration mapConfig, PlanetType planetType, MapTypeData mapType)
         {
-            int numberToSeed = mapConfig.GetClampedNumberOfPlanetsForMapType(mapType);
-            int numberOfRows = 9;
-            int numberOfColumns = 16;
-            int distanceBetweenPoints = planetType.GetData().InterStellarRadius * 4;
+            int numPlanets = mapConfig.GetClampedNumberOfPlanetsForMapType(mapType);
+            int tessellation = BadgerUtilityMethods.getSettingValueMapSettingOptionChoice_Expensive(mapConfig, "Tessellation").RelatedIntValue;
+            int mapShapeEnum = BadgerUtilityMethods.getSettingValueMapSettingOptionChoice_Expensive(mapConfig, "AspectRatio").RelatedIntValue;
+            FInt aspectRatio;
+            if (mapShapeEnum == 0)
+            {
+                aspectRatio = FInt.FromParts(0, 625);
+            }
+            else if (mapShapeEnum == 1)
+            {
+                aspectRatio = FInt.FromParts(1, 0);
+            }
+            else
+            {
+                aspectRatio = FInt.FromParts(1, 778);
+            }
+            FakeGalaxy g;
+            if (tessellation == 0)
+            {
+                g = MakeSquareGalaxy(planetType, aspectRatio, numPlanets);
+            }
+            else
+            {
+                g = MakeSquareYGalaxy(planetType, aspectRatio, numPlanets);
+            }
+            g.Populate(galaxy, planetType, Context.RandomToUse);
+
+            // FIXME
+            int randomExtraConnections = BadgerUtilityMethods.getSettingValueMapSettingOptionChoice_Expensive(mapConfig, "Dissonance").RelatedIntValue * 5;
+            BadgerUtilityMethods.RandomlyConnectXPlanetsWithoutIntersectingOthers(galaxy, randomExtraConnections, 40, 20, Context);
+
+            BadgerUtilityMethods.makeSureGalaxyIsFullyConnected(true, galaxy);
+        }
+
+        protected FakeGalaxy MakeSquareGalaxy(PlanetType planetType, FInt aspectRatio, int numPlanets)
+        {
+            int unit = planetType.GetData().InterStellarRadius * 10;
+            int rows = 9;
+            int columns = 16;
+            FInt badness = (FInt)1000000;
+            for (int r = 1; r <= 30; ++r)
+            {
+                for (int c = 1; c <= 30; ++c)
+                {
+                    int planets = r * c;
+                    FInt planetBadness = (FInt)Math.Abs(planets - numPlanets);
+                    FInt currentAspectRatio = (FInt)r / (FInt)c;
+                    FInt p1 = currentAspectRatio / aspectRatio;
+                    FInt p2 = aspectRatio / currentAspectRatio;
+                    FInt aspectRatioBadness = ((p1 > p2 ? p1 : p2) - FInt.One) * (FInt)10;
+                    FInt current_badness = planetBadness + aspectRatioBadness;
+                    if (current_badness < badness)
+                    {
+                        badness = current_badness;
+                        rows = r;
+                        columns = c;
+                    }
+                }
+            }
             FakeGalaxy g = new FakeGalaxy();
 
-            FakePlanet[][] pointRows = new FakePlanet[numberOfRows][];
-            for (int i = 0; i < numberOfRows; ++i)
+            FakePlanet[][] pointRows = new FakePlanet[rows][];
+            for (int i = 0; i < rows; ++i)
             {
-                pointRows[i] = new FakePlanet[numberOfColumns];
-                for (int j = 0; j < numberOfColumns; ++j)
+                pointRows[i] = new FakePlanet[columns];
+                for (int j = 0; j < columns; ++j)
                 {
-                    pointRows[i][j] = g.AddPlanetAt(ArcenPoint.Create(j * distanceBetweenPoints, i * distanceBetweenPoints));
+                    pointRows[i][j] = g.AddPlanetAt(ArcenPoint.Create(j * unit, i * unit));
                 }
             }
 
@@ -58,14 +114,81 @@ namespace AhyangyiMaps
                     }
                 }
             }
-
-            g.Populate(galaxy, planetType, Context.RandomToUse);
-
-            // FIXME
-            int randomExtraConnections = BadgerUtilityMethods.getSettingValueMapSettingOptionChoice_Expensive(mapConfig, "Dissonance").RelatedIntValue * 5;
-            BadgerUtilityMethods.RandomlyConnectXPlanetsWithoutIntersectingOthers(galaxy, randomExtraConnections, 40, 20, Context);
-
-            BadgerUtilityMethods.makeSureGalaxyIsFullyConnected(true, galaxy);
+            return g;
         }
-    }
+
+        protected FakeGalaxy MakeSquareYGalaxy(PlanetType planetType, FInt aspectRatio, int numPlanets)
+        {
+            int unit = planetType.GetData().InterStellarRadius * 10;
+            int rows = 5;
+            int columns = 8;
+            FInt badness = (FInt)1000000;
+            for (int r = 1; r <= 20; ++r)
+            {
+                for (int c = 1; c <= 20; ++c)
+                {
+                    int planets = r * c + (r - 1) * (c - 1) * 2;
+                    FInt planetBadness = (FInt)Math.Abs(planets - numPlanets);
+                    FInt currentAspectRatio = (FInt)r / (FInt)c;
+                    FInt p1 = currentAspectRatio / aspectRatio;
+                    FInt p2 = aspectRatio / currentAspectRatio;
+                    FInt aspectRatioBadness = ((p1 > p2 ? p1 : p2) - FInt.One) * (FInt)10;
+                    FInt current_badness = planetBadness + aspectRatioBadness;
+                    if (current_badness < badness)
+                    {
+                        badness = current_badness;
+                        rows = r;
+                        columns = c;
+                    }
+                }
+            }
+            FakeGalaxy g = new FakeGalaxy();
+
+            FakePlanet[][] corners = new FakePlanet[rows][];
+            FakePlanet[][] centers = new FakePlanet[rows - 1][];
+            FakePlanet[][] bottoms = new FakePlanet[rows - 1][];
+            for (int i = 0; i < rows; ++i)
+            {
+                corners[i] = new FakePlanet[columns];
+                if (i + 1 < rows)
+                {
+                    centers[i] = new FakePlanet[columns - 1];
+                    bottoms[i] = new FakePlanet[columns - 1];
+                }
+                for (int j = 0; j < columns; ++j)
+                {
+                    corners[i][j] = g.AddPlanetAt(ArcenPoint.Create(j * 2 * unit, (rows - i - 1) * 2 * unit));
+                    if (i + 1 < rows && j + 1 < columns)
+                    {
+                        centers[i][j] = g.AddPlanetAt(ArcenPoint.Create((j * 2 + 1) * unit, ((rows - i - 1) * 2 - 1) * unit));
+                        bottoms[i][j] = g.AddPlanetAt(ArcenPoint.Create((j * 2 + 1) * unit, ((rows - i - 1) * 2 - 2) * unit));
+                    }
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (i + 1 < rows)
+                    {
+                        corners[i][j].AddLinkTo(corners[i + 1][j]);
+                    }
+                    if (i == 0 && j + 1 < columns)
+                    {
+                        corners[i][j].AddLinkTo(corners[i][j + 1]);
+                    }
+                    if (i + 1 < rows && j + 1 < columns)
+                    {
+                        corners[i][j].AddLinkTo(centers[i][j]);
+                        corners[i][j + 1].AddLinkTo(centers[i][j]);
+                        centers[i][j].AddLinkTo(bottoms[i][j]);
+                        corners[i + 1][j].AddLinkTo(bottoms[i][j]);
+                        corners[i + 1][j + 1].AddLinkTo(bottoms[i][j]);
+                    }
+                }
+            }
+            return g;
+        }
+   }
 }
