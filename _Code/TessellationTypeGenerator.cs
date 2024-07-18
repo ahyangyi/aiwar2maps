2688b50617ace1285958b37ce57e9455fe10f797
Yi Yang @ Ahlfeldite <ahyangyi@gmail.com>
2024-07-18 13:14:26 +0800

    (tes) triangles

--- _Code/TessellationTypeGenerator.cs
+++ _Code/TessellationTypeGenerator.cs
@@ -51,6 +51,10 @@ namespace AhyangyiMaps
             {
                 g = MakeHexagonGalaxy(planetType, aspectRatio, numPlanets);
             }
+            else if (tessellation == 2)
+            {
+                g = MakeTriangleGalaxy(planetType, aspectRatio, numPlanets);
+            }
             else
             {
                 g = MakeSquareYGalaxy(planetType, aspectRatio, numPlanets);
@@ -70,13 +74,13 @@ namespace AhyangyiMaps
             int rows = 9;
             int columns = 16;
             FInt badness = (FInt)1000000;
-            for (int r = 1; r <= 30; ++r)
+            for (int r = 2; r <= 30; ++r)
             {
-                for (int c = 1; c <= 30; ++c)
+                for (int c = 2; c <= 30; ++c)
                 {
                     int planets = r * c;
                     FInt planetBadness = (FInt)Math.Abs(planets - numPlanets);
-                    FInt currentAspectRatio = (FInt)r / (FInt)c;
+                    FInt currentAspectRatio = (FInt)(r - 1) / (FInt)(c - 1);
                     FInt p1 = currentAspectRatio / aspectRatio;
                     FInt p2 = aspectRatio / currentAspectRatio;
                     FInt aspectRatioBadness = ((p1 > p2 ? p1 : p2) - FInt.One) * (FInt)10;
@@ -146,8 +150,8 @@ namespace AhyangyiMaps
                 }
             }
             FakeGalaxy g = new FakeGalaxy();
-            System.Collections.Generic.Dictionary<Pair<int, int>, FakePlanet> uppy = new System.Collections.Generic.Dictionary<Pair<int, int>, FakePlanet>();
-            System.Collections.Generic.Dictionary<Pair<int, int>, FakePlanet> downy = new System.Collections.Generic.Dictionary<Pair<int, int>, FakePlanet>();
+            System.Collections.Generic.Dictionary<(int, int), FakePlanet> uppy = new System.Collections.Generic.Dictionary<(int, int), FakePlanet>();
+            System.Collections.Generic.Dictionary<(int, int), FakePlanet> downy = new System.Collections.Generic.Dictionary<(int, int), FakePlanet>();
 
             for (int i = 0; i <= rows; ++i)
             {
@@ -156,11 +160,11 @@ namespace AhyangyiMaps
                     if (i >= 0 && i < rows && j >= 0 && j < columns ||
                         i >= 0 && i < rows && j - 2 >= 0 && j - 2 < columns ||
                         i - 1 >= 0 && i - 1 < rows && j - 1 >= 0 && j - 1 < columns)
-                        uppy[Pair<int, int>.Create(i, j)] = g.AddPlanetAt(ArcenPoint.Create(j * xunit, (i * 3 + 1) * yunit));
+                        uppy[(i, j)] = g.AddPlanetAt(ArcenPoint.Create(j * xunit, (i * 3 + 1) * yunit));
                     if (i >= 0 && i < rows && j >= 0 && j < columns ||
                         i - 1 >= 0 && i - 1 < rows && j - 1 >= 0 && j - 1 < columns ||
                         i - 1 >= 0 && i - 1 < rows && j + 1 >= 0 && j + 1 < columns)
-                        downy[Pair<int, int>.Create(i, j)] = g.AddPlanetAt(ArcenPoint.Create((j + 1) * xunit, (i * 3) * yunit));
+                        downy[(i, j)] = g.AddPlanetAt(ArcenPoint.Create((j + 1) * xunit, (i * 3) * yunit));
                 }
             }
 
@@ -168,38 +172,96 @@ namespace AhyangyiMaps
             {
                 for (int j = -i % 2; j <= columns + 1; j += 2)
                 {
-                    if (uppy.ContainsKey(Pair<int, int>.Create(i, j)))
+                    if (uppy.ContainsKey((i, j)))
                     {
-                        if (downy.ContainsKey(Pair<int, int>.Create(i, j)))
+                        if (downy.ContainsKey((i, j)))
                         {
-                            uppy[Pair<int, int>.Create(i, j)].AddLinkTo(downy[Pair<int, int>.Create(i, j)]);
+                            uppy[(i, j)].AddLinkTo(downy[(i, j)]);
                         }
-                        if (downy.ContainsKey(Pair<int, int>.Create(i, j - 2)))
+                        if (downy.ContainsKey((i, j - 2)))
                         {
-                            uppy[Pair<int, int>.Create(i, j)].AddLinkTo(downy[Pair<int, int>.Create(i, j - 2)]);
+                            uppy[(i, j)].AddLinkTo(downy[(i, j - 2)]);
                         }
-                        if (downy.ContainsKey(Pair<int, int>.Create(i + 1, j - 1)))
+                        if (downy.ContainsKey((i + 1, j - 1)))
                         {
-                            uppy[Pair<int, int>.Create(i, j)].AddLinkTo(downy[Pair<int, int>.Create(i + 1, j - 1)]);
+                            uppy[(i, j)].AddLinkTo(downy[(i + 1, j - 1)]);
                         }
                     }
                 }
             }
             return g;
         }
+
+        // FIXME move to seperate namespace/class
+        readonly int[] dr = { 1, 2, 1, -1, -2, -1 };
+        readonly int[] dc = { -1, 0, 1, 1, 0, -1 };
+        protected FakeGalaxy MakeTriangleGalaxy(PlanetType planetType, FInt aspectRatio, int numPlanets)
+        {
+            int xunit = planetType.GetData().InterStellarRadius * 8660 / 1000;
+            int yunit = planetType.GetData().InterStellarRadius * 5;
+            int rows = 9;
+            int columns = 16;
+            FInt badness = (FInt)1000000;
+            for (int r = 2; r <= 25; ++r)
+            {
+                for (int c = 2; c <= 40; ++c)
+                {
+                    // FIXME: rough estimation
+                    int planets = (r * c) / 2;
+                    FInt planetBadness = (FInt)Math.Abs(planets - numPlanets);
+                    FInt currentAspectRatio = ((FInt)r - 1) * yunit / (((FInt)c - 1) * xunit);
+                    FInt p1 = currentAspectRatio / aspectRatio;
+                    FInt p2 = aspectRatio / currentAspectRatio;
+                    FInt aspectRatioBadness = ((p1 > p2 ? p1 : p2) - FInt.One) * (FInt)10;
+                    FInt current_badness = planetBadness + aspectRatioBadness;
+                    if (current_badness < badness)
+                    {
+                        badness = current_badness;
+                        rows = r;
+                        columns = c;
+                    }
+                }
+            }
+            FakeGalaxy g = new FakeGalaxy();
+            System.Collections.Generic.Dictionary<(int, int), FakePlanet> points = new System.Collections.Generic.Dictionary<(int, int), FakePlanet>();
+
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = (i + 1) % 2; j < columns; j += 2)
+                {
+                    points[(i, j)] = g.AddPlanetAt(ArcenPoint.Create(j * xunit, i * yunit));
+                }
+            }
+
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = (i + 1) % 2; j < columns; j += 2)
+                {
+                    for (int d = 0; d < 6; ++d)
+                    {
+                        if (i + dr[d] >= 0 && i + dr[d] < rows && j + dc[d] >= 0 && j + dc[d] < columns)
+                        {
+                            points[(i, j)].AddLinkTo(points[(i + dr[d], j + dc[d])]);
+                        }
+                    }
+                }
+            }
+            return g;
+        }
+
         protected FakeGalaxy MakeSquareYGalaxy(PlanetType planetType, FInt aspectRatio, int numPlanets)
         {
             int unit = planetType.GetData().InterStellarRadius * 10;
             int rows = 5;
             int columns = 8;
             FInt badness = (FInt)1000000;
-            for (int r = 1; r <= 20; ++r)
+            for (int r = 2; r <= 20; ++r)
             {
-                for (int c = 1; c <= 20; ++c)
+                for (int c = 2; c <= 20; ++c)
                 {
                     int planets = r * c + (r - 1) * (c - 1) * 2;
                     FInt planetBadness = (FInt)Math.Abs(planets - numPlanets);
-                    FInt currentAspectRatio = (FInt)r / (FInt)c;
+                    FInt currentAspectRatio = (FInt)(r - 1) / (FInt)(c - 1);
                     FInt p1 = currentAspectRatio / aspectRatio;
                     FInt p2 = aspectRatio / currentAspectRatio;
                     FInt aspectRatioBadness = ((p1 > p2 ? p1 : p2) - FInt.One) * (FInt)10;
