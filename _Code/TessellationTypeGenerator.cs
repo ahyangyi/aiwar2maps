68daa3d78309a5dad54eddddd784066f1af2562a
Yi Yang @ Ahlfeldite <ahyangyi@gmail.com>
2024-07-20 18:08:29 +0800

    (tes) add flowery diamond type

--- _Code/TessellationTypeGenerator.cs
+++ _Code/TessellationTypeGenerator.cs
@@ -65,23 +65,30 @@ namespace AhyangyiMaps
             {
                 g = MakeSquareYMirrorGalaxy(planetType, aspectRatio, numPlanetsToMake);
             }
-            else
+            else if (tessellation == 102)
             {
                 g = MakeDiamondYGalaxy(planetType, aspectRatio, numPlanetsToMake);
+            }
+            else
+            {
+                g = MakeDiamondYFlowerGalaxy(planetType, aspectRatio, numPlanetsToMake);
             }
 
             int wobble = BadgerUtilityMethods.getSettingValueMapSettingOptionChoice_Expensive(mapConfig, "Wobble").RelatedIntValue;
             g.Wobble(planetType, wobble, Context.RandomToUse);
 
-            while (g.planets.Count > numPlanets)
+            if (dissonance > 0)
             {
-                FakePlanet j = g.primaryPlanets[Context.RandomToUse.Next(0, g.primaryPlanets.Count - 1)];
-                g.RemovePlanetAndCounterparts(j);
+                while (g.planets.Count > numPlanets)
+                {
+                    FakePlanet j = g.primaryPlanets[Context.RandomToUse.Next(0, g.primaryPlanets.Count - 1)];
+                    g.RemovePlanetAndCounterparts(j);
+                }
             }
 
             g.Populate(galaxy, planetType, Context.RandomToUse);
 
-            BadgerUtilityMethods.makeSureGalaxyIsFullyConnected(true, galaxy);
+            //BadgerUtilityMethods.makeSureGalaxyIsFullyConnected(true, galaxy);
         }
 
         protected FakeGalaxy MakeSquareGalaxy(PlanetType planetType, FInt aspectRatio, int galaxyLayout, int numPlanets)
@@ -551,6 +558,117 @@ namespace AhyangyiMaps
                     }
                 }
             }
+            return g;
+        }
+        protected FakeGalaxy MakeDiamondYFlowerGalaxy(PlanetType planetType, FInt aspectRatio, int numPlanets)
+        {
+            // FIXME move to seperate namespace/class
+            int[] dr = { 1, 2, 1, 0, -1, -2, -1, 0 };
+            int[] dc = { 1, 0, -1, -2, -1, 0, 1, 2 };
+
+            int unit = planetType.GetData().InterStellarRadius * 7071 / 1000;
+            int rows = 5;
+            int columns = 8;
+            FInt badness = (FInt)1000000;
+            for (int r = 3; r <= 20; r += 2)
+            {
+                for (int c = 3; c <= 20; c += 2)
+                {
+                    // FIXME: simple estimation
+                    int planets = r * c * 2;
+                    FInt planetBadness = (FInt)Math.Abs(planets - numPlanets);
+                    FInt currentAspectRatio = (FInt)(r + 1) / (FInt)(c + 1);
+                    FInt p1 = currentAspectRatio / aspectRatio;
+                    FInt p2 = aspectRatio / currentAspectRatio;
+                    FInt aspectRatioBadness = ((p1 > p2 ? p1 : p2) - FInt.One) * (FInt)10;
+                    FInt current_badness = planetBadness + aspectRatioBadness;
+                    if (current_badness < badness)
+                    {
+                        badness = current_badness;
+                        rows = r;
+                        columns = c;
+                    }
+                }
+            }
+            FakeGalaxy g = new FakeGalaxy();
+            System.Collections.Generic.Dictionary<(int, int), FakePlanet> points = new System.Collections.Generic.Dictionary<(int, int), FakePlanet>();
+            for (int i = 0; i < rows + 2; ++i)
+            {
+                for (int j = 0; j < columns + 2; ++j)
+                {
+                    if ((i + j) % 2 == 0 && (i > 0 && i <= rows || j > 0 && j <= columns))
+                    {
+                        points[(i * 2, j * 2)] = g.AddPlanetAt(ArcenPoint.Create(j * 2 * unit, i * 2 * unit));
+                    }
+                }
+            }
+            for (int i = 0; i < rows + 2; ++i)
+            {
+                for (int j = 0; j < columns + 2; ++j)
+                {
+                    if (i % 2 == 0 && j % 2 == 0 && points.ContainsKey((i * 2, j * 2)) && (i + j) % 4 == 0)
+                    {
+                        for (int d = 0; d < 8; ++d)
+                        {
+                            if (points.ContainsKey((i * 2 + dr[d] * 2, j * 2 + dc[d] * 2))
+                                && (points.ContainsKey((i * 2 + dr[(d + 1) % 8] * 2, j * 2 + dc[(d + 1) % 8] * 2))
+                                && points.ContainsKey((i * 2 + dr[(d + 7) % 8] * 2, j * 2 + dc[(d + 7) % 8] * 2)) || d % 2 == 0))
+                            {
+                                points[(i * 2 + dr[d], j * 2 + dc[d])] = g.AddPlanetAt(ArcenPoint.Create((j * 2 + dc[d]) * unit, (i * 2 + dr[d]) * unit));
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < rows + 2; ++i)
+            {
+                for (int j = 0; j < columns + 2; ++j)
+                {
+                    if ((i + j) % 2 == 0 && points.ContainsKey((i * 2, j * 2)))
+                    {
+                        if (points.ContainsKey((i * 2 + 1, j * 2 - 1)))
+                        {
+                            points[(i * 2, j * 2)].AddLinkTo(points[(i * 2 + 1, j * 2 - 1)]);
+                            if (points.ContainsKey((i * 2 + 2, j * 2 - 2)))
+                                points[(i * 2 + 1, j * 2 - 1)].AddLinkTo(points[(i * 2 + 2, j * 2 - 2)]);
+                        }
+                        else if (points.ContainsKey((i * 2 + 2, j * 2 - 2)))
+                        {
+                            points[(i * 2, j * 2)].AddLinkTo(points[(i * 2 + 2, j * 2 - 2)]);
+                        }
+                        if (points.ContainsKey((i * 2 + 1, j * 2 + 1)))
+                        {
+                            points[(i * 2, j * 2)].AddLinkTo(points[(i * 2 + 1, j * 2 + 1)]);
+                            if (points.ContainsKey((i * 2 + 2, j * 2 + 2)))
+                                points[(i * 2 + 1, j * 2 + 1)].AddLinkTo(points[(i * 2 + 2, j * 2 + 2)]);
+                        }
+                        else if (points.ContainsKey((i * 2 + 2, j * 2 + 2)))
+                        {
+                            points[(i * 2, j * 2)].AddLinkTo(points[(i * 2 + 2, j * 2 + 2)]);
+                        }
+
+                        if (i % 2 == 0 && j % 2 == 0 && (i + j) % 4 == 0)
+                        {
+                            for (int d = 0; d < 8; ++d)
+                            {
+                                if (points.ContainsKey((i * 2 + dr[d], j * 2 + dc[d])))
+                                {
+                                    if (d % 2 == 1)
+                                    {
+                                        points[(i * 2 + dr[d], j * 2 + dc[d])].AddLinkTo(points[(i * 2 + dr[d] * 2, j * 2 + dc[d] * 2)]);
+                                    }
+                                    if (points.ContainsKey((i * 2 + dr[(d + 1) % 8], j * 2 + dc[(d + 1) % 8])))
+                                    {
+                                        points[(i * 2 + dr[d], j * 2 + dc[d])].AddLinkTo(points[(i * 2 + dr[(d + 1) % 8], j * 2 + dc[(d + 1) % 8])]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             return g;
         }
     }
