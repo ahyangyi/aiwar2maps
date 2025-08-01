6564e0df9e9312a4bac92eb32b9e1ae247bc5fa6
Yi Yang @ Ametrine <ahyangyi@gmail.com>
2025-08-02 03:36:21 +0800

    (tes) implement juxtaposed' SquareY

--- _Code/Tessellation/SquareYGrid.cs
+++ _Code/Tessellation/SquareYGrid.cs
@@ -312,7 +312,7 @@ namespace AhyangyiMaps.Tessellation
             }
             else if (galaxyShape == 1)
             {
-                g = MakeGridSemioctagonal(rows, columns, (symmetry >= 200 && symmetry <= 250 || symmetry == 10002) ? 1 : 0, sp);
+                g = MakeGridSemioctagonal(rows, columns, (symmetry >= 200 && symmetry <= 250 || symmetry == 10002) ? 1 : 0, sp, f, offset);
             }
             else
             {
@@ -390,20 +390,24 @@ namespace AhyangyiMaps.Tessellation
             return g;
         }
 
-        protected static FakeGalaxy MakeGridSemioctagonal(int rows, int columns, int flip, int octagonalSideLength)
+        protected static FakeGalaxy MakeGridSemioctagonal(int rows, int columns, int flip, int octagonalSideLength, int sectionColumns = 0, int sectionOffset = 0)
         {
             FakeGalaxy g = new FakeGalaxy();
+            if (sectionColumns == 0)
+            {
+                sectionColumns = sectionOffset = columns;
+            }
             for (int i = 0; i < rows; ++i)
                 for (int j = 0; j < columns; ++j)
                 {
                     var curSpare = (flip == 2 || flip == 1 && i < rows / 2 ? squareYFlipped : squareY);
-                    int k = j % columns;
+                    int k = j % sectionOffset % sectionColumns;
                     if ((i + k) < octagonalSideLength) continue;
-                    if ((i + columns - 1 - k) < octagonalSideLength) continue;
+                    if ((i + sectionColumns - 1 - k) < octagonalSideLength) continue;
                     if (flip == 1)
                     {
                         if ((rows - 1 - i + k) < octagonalSideLength) continue;
-                        if ((rows - 1 - i + columns - 1 - k) < octagonalSideLength) continue;
+                        if ((rows - 1 - i + sectionColumns - 1 - k) < octagonalSideLength) continue;
                     }
                     curSpare.Imprint(g, ArcenPoint.Create(j * unit * 2, i * unit * 2));
                 }
